9d72b802ba41eab27d520a8a1594cc328786b675
dburea01 <dburea01@gmail.com>
2022-07-10 17:02:54 +0200

    sonar cloud

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\PostalCodeController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,25 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::get('/postal_codes', [PostalCodeController::class, 'index']);
-});
-
-/**
- * Healthcheck.
- *
- * Check that the service is up. If everything is okay, you'll get a 200 OK response.
- *
- * Otherwise, the request will fail with a 400 error, and a response listing the failed services.
- *
- * @response 400 scenario="Service is unhealthy" {"status": "down", "services": {"database": "up", "redis": "down"}}
- * @responseField status The status of this API (`up` or `down`).
- * @responseField services Map of each downstream service and their status (`up` or `down`).
- */
-Route::get('/healthcheck', function () {
-    return [
-        'status' => 'up',
-        'services' => [
-            'database' => 'up',
-            'redis' => 'up',
-        ],
-    ];
 });
